4e615d7d06ff7899fc11edcf75cbd0e831ba23dd
takehiro.nakamori <takehiro.nakamori@dena.jp>
2019-12-16 15:00:39 +0900

    数字モード変更

--- keyboards/mint60/keymaps/hilo/keymap.c
+++ keyboards/mint60/keymaps/hilo/keymap.c
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT( \
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL, \
     _______,   _______,  KC_UP, KC_UP, _______, XXXXXXX,                          XXXXXXX,        KC_7,       KC_8,      KC_9,    XXXXXXX, XXXXXXX, XXXXXXX, JP_PIPE, \
-    KC_CAPS,     KC_LEFT, KC_DOWN, KC_RGHT, _______, XXXXXXX,                      XXXXXXX,     KC_4,        RSFT_T(KC_5),        ALT_T(KC_6), XXXXXXX, KC_GRV,        XXXXXXX, \
+    KC_CAPS,     KC_LEFT, KC_DOWN, KC_RGHT, _______, XXXXXXX,                      XXXXXXX,     KC_4,        RSFT_T(KC_5),        ALT_T(KC_6), KC_COLON, KC_GRV,        XXXXXXX, \
     _______,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                              XXXXXXX,    KC_1,      KC_2,        KC_3   , XXXXXXX, KC_TILDE, KC_PGUP, _______, \
-    XXXXXXX,   _______,    _______,    _______,    KC_LANG2,                                              GUI_T(KC_0),      KC_0,         XXXXXXX,                                           KC_HOME, KC_PGDN, KC_END \
+    XXXXXXX,   _______,    _______,    _______,    KC_LANG2,                                              GUI_T(KC_0),      KC_0,         KC_DOT,                                           KC_HOME, KC_PGDN, KC_END \
   ),
   [2] = LAYOUT( \
       XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, \
