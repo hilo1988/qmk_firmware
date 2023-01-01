337b47f459ce1d2ece4e77860fec64570c2df06c
takehiro.nakamori <takehiro.nakamori@dena.com>
2023-01-01 19:24:16 +0900

    base42 towel

--- keyboards/ergodash/rev1/keymaps/hilo/keymap.c
+++ keyboards/ergodash/rev1/keymaps/hilo/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,                KC_5,    EISU,                                          KANA,                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,        KC_BSPC, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                KC_T,    LT(_RAISE, KC_SPC),                            LT(_RAISE, KC_SPC) , KC_Y,    KC_U,    KC_I,    KC_O,    CC_T(KC_P),  KC_ENT, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    LT(_TENKEY, KC_F),   KC_G,    LT(_LOWER, KC_SPC) ,                           LT(_LOWER, KC_SPC),  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,     KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,                KC_B,                        SGUI_T(KC_SPC),            SGUI_T(KC_SPC) ,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,     KC_GRV, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,                KC_B,    SGUI_T(KC_SPC),            SGUI_T(KC_SPC) ,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,     KC_GRV, \
     KC_LCTL, KC_LGUI, KC_LALT, KC_LCTL,                      EISU,    LT(_LOWER, KC_SPC) ,KC_SPC,                    KC_SPC, LT(_RAISE, KC_SPC) , KANA,            KC_LEFT, KC_DOWN, KC_UP,       KC_RGHT  \
   ),
 
